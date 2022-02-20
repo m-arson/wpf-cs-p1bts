51ce5f1dc25c93036812853feeece7c2f5792137
m-arson <arsonmarianus@gmail.com>
2022-02-20 16:36:26 +0800

    Fixing Datagrid Scrolling Issue

---
+++ p1BTS/DataGridItem.cs
@@ -0,0 +1,34 @@
+﻿namespace p1BTS
+{
+    public class DataGridItem
+    {
+        public string? no { get; set; }
+        public string? so_numb { get; set; }
+        public string? site_id { get; set; }
+        public string? site_id_op { get; set; }
+        public string? site_name { get; set; }
+        public string? company { get; set; }
+        public string? status { get; set; }
+        public string? operatr { get; set; }
+        public string? tower_height { get; set; }
+        public string? kom_sitac_date { get; set; }
+        public string? project { get; set; }
+        public string? validasi_date { get; set; }
+        public string? long_ { get; set; }
+        public string? lat { get; set; }
+        public string? residence { get; set; }
+        public string? province { get; set; }
+        public string? spk_pre_sitac { get; set; }
+        public string? po_pre_sitac { get; set; }
+        public string? status_bast { get; set; }
+        public string? po_sitac { get; set; }
+        public string? po_cme { get; set; }
+        public string? status_bast2 { get; set; }
+        public string? po_operasional { get; set; }
+        public string? po_date { get; set; }
+        public string? status_ { get; set; }
+        public string? po_comcase { get; set; }
+        public string? remark_bast_cme { get; set; }
+        public string? remark_bast_imb { get; set; }
+    }
+}
